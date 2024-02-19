f75e58bad80af01cc84269e231f51485a99a21d1
HoustonBC <houstoncharles@Houstons-MacBook-Air.local>
2024-02-19 18:20:30 -0500

    restrict functionality to reddit

--- urlSwitcher.js
+++ urlSwitcher.js
@@ -3,19 +3,22 @@ chrome.action.onClicked.addListener((tab) => {
         if (tabs.length > 0) {
             let url = tabs[0].url;
             let tabId = tabs[0].id;
-            if (url.includes('www')) {
-                newUrl = url.replace('www', 'old');
-                chrome.tabs.update(tabId, { url: newUrl });
-                chrome.action.setIcon({ path: "icons/old128.png" });
-            } else if (url.includes('new')) {
-                newUrl = url.replace('new', 'old');
-                chrome.tabs.update(tabId, { url: newUrl });
-                chrome.action.setIcon({ path: "icons/old128.png" });
-            } else if (url.includes('old')) {
-                newUrl = url.replace('old', 'new');
-                chrome.tabs.update(tabId, { url: newUrl });
-                chrome.action.setIcon({ path: "icons/new128.png" });
+            if (url.includes('reddit.com')) {
+                if (url.includes('www')) {
+                    newUrl = url.replace('www', 'old');
+                    chrome.tabs.update(tabId, { url: newUrl });
+                    chrome.action.setIcon({ path: "icons/old128.png" });
+                } else if (url.includes('new')) {
+                    newUrl = url.replace('new', 'old');
+                    chrome.tabs.update(tabId, { url: newUrl });
+                    chrome.action.setIcon({ path: "icons/old128.png" });
+                } else if (url.includes('old')) {
+                    newUrl = url.replace('old', 'new');
+                    chrome.tabs.update(tabId, { url: newUrl });
+                    chrome.action.setIcon({ path: "icons/new128.png" });
+                } 
             }
+            
         }
     })
 })
